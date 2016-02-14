b74d2609f0d98c15c1f7090427bdc4f8095db39d
Tim Leerhoff <tleerhof@web.de>
2016-02-14 14:12:17 +0100

    Added tests.

--- test/radardetection.js
+++ test/radardetection.js
@@ -3,11 +3,43 @@ var vecmat = require('lethexa-vecmat');
 var radardetect = require((process.env.APP_DIR_FOR_CODE_COVERAGE || '../lib/') + 'radardetection.js');
 
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal(-1, [1, 2, 3].indexOf(5));
-            assert.equal(-1, [1, 2, 3].indexOf(0));
+describe('RadarSender', function () {
+    describe('when sendpower=125663.70614W and distance=1000.0m', function () {
+        it('should return power density of 0.00999999999971418', function () {
+		var sender = new radardetect.RadarSender({
+    			sndPower: 125663.70614, // 4*PI*10000.0
+    			antennaGain: 1.0,
+    			patternPropFactor: 1.0
+		});
+
+		var targetDistanceSquared = Math.pow(1000.0, 2.0);
+		var powerDensityAtTarget = sender.calcPowerDensityAtTarget(
+			targetDistanceSquared
+		);
+
+		assert.equal(0.00999999999971418, powerDensityAtTarget);
         });
     });
 });
+
+
+describe('RadarReceiver', function () {
+    describe('when reflectpower=125663.70614W, target crosssection=1m and distance=1000.0m', function () {
+        it('should return power density of 0.00999999999971418', function () {
+		var sender = new radardetect.RadarSender({
+    			sndPower: 125663.70614, // 4*PI*10000.0
+    			antennaGain: 1.0,
+    			patternPropFactor: 1.0
+		});
+
+		var targetDistanceSquared = Math.pow(1000.0, 2.0);
+		var powerDensityAtTarget = sender.calcPowerDensityAtTarget(
+			targetDistanceSquared,
+			1.0
+		);
+
+		assert.equal(0.00999999999971418, powerDensityAtTarget);
+        });
+    });
+});
+
